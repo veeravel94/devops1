93e05d63807bc7bb16f88a4520a7fa8435c59a83
veeravel94 <veeravelvelu@live.com>
2018-07-19 18:33:12 +0530

    second commit

--- p1.py
+++ p1.py
@@ -8,3 +8,7 @@ sum = float(num1) + float(num2)
 
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+deff = float(num1) - float(num2) 
+
+print('the sum of {0} and {1} is {2}'.format (num1, num2, deff))
